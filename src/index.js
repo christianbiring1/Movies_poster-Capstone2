6b003cf38f292bacd0f4354904b1008b7176204b
christian <christianbiringanine22@gmail.com>
2022-05-20 16:20:20 +0200

    Updating the likes on the movies

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 // import './modules/home.js';
 import generateUIElement, { get, sendLike, getLike } from './modules/home.js';
 
-window.addEventListener('load', async () => {
+const displayItem = async () => {
   const movies = await get();
   generateUIElement(movies);
 
@@ -11,15 +11,31 @@ window.addEventListener('load', async () => {
     btn.addEventListener('click', async (e) => {
       const element = e.target.nextElementSibling;
       const targetId = element.id;
-      for (let i = 0; i < movies.length; i += 1) {
-        if (movies[i].id === targetId) {
-          sendLike(movies[i].id);
+      movies.forEach((movie) => {
+        if (parseInt(targetId, 10) === movie.id) {
+          sendLike(movie.id);
+          getLike().then((result) => {
+            result.filter((item) => {
+              if (item.item_id === movie.id) {
+                element.textContent = `${item.likes} likes`;
+              }
+              return item;
+            });
+          });
         }
-      }
-      // await sendLike();
-      // getLike().then((res) => {
-      //   element.textContent = `${res.likes} likes`;
-      // });
+      });
     });
   });
-});
+  const likeCount = document.querySelectorAll('.likecount');
+  likeCount.forEach((el) => {
+    console.log(parseInt(el.id, 10));
+    const likeUpdate = async () => {
+      const likes = await getLike();
+      if (parseInt(el.id, 10) === likes.item_id) {
+        el.textContent = `${likes.likes} likes`;
+      }
+    };
+    likeUpdate();
+  });
+};
+displayItem();
