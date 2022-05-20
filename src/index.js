aacad6ab99bca161c1ce98c447736e32a55b7b4e
christian <christianbiringanine22@gmail.com>
2022-05-20 12:57:46 +0200

    Fix the bug

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import './style.css';
-import './modules/home.js';
+// import './modules/home.js';
+import generateUIElement, { get, sendLike, getLike } from './modules/home.js';
+
+window.addEventListener('load', async () => {
+  const movies = await get();
+  generateUIElement(movies);
+
+  const likebtn = document.querySelectorAll('i');
+  likebtn.forEach((btn) => {
+    btn.addEventListener('click', async (e) => {
+      const element = e.target.nextElementSibling;
+      const targetId = element.id;
+      for (let i = 0; i < movies.length; i += 1) {
+        if (movies[i].id === targetId) {
+          sendLike(movies[i].id);
+        }
+      }
+      // await sendLike();
+      // getLike().then((res) => {
+      //   element.textContent = `${res.likes} likes`;
+      // });
+    });
+  });
+});
